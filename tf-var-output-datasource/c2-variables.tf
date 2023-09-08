a23f4703b5fc2710288a67f6c024b6372dc55f7b
vishu-25 <chadhavismay25@gmail.com>
2023-09-08 20:36:20 +0530

    removing keypair name

--- tf-var-output-datasource/c2-variables.tf
+++ tf-var-output-datasource/c2-variables.tf
@@ -13,5 +13,5 @@ variable "instance_type" {
 variable "instance_keypair" {
   description = "RSA key for SSH"
   type = string
-  default = "terraform-aws"
+  default = " "       # Add your keypair here 
 }
