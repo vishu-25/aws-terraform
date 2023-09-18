bfc81cbdad2e63cfcc08c5493bb3b02bf1539431
vishu-25 <chadhavismay25@gmail.com>
2023-09-18 16:39:55 +0530

    add c2-v2 file

--- terraform-utility-project/c2-v1-get-instance-type-per-az.tf
+++ terraform-utility-project/c2-v1-get-instance-type-per-az.tf
@@ -1,4 +1,4 @@
-/*
+
 data "aws_ec2_instance_type_offerings" "my-instance-type-1" {
   filter {
     name = "instance-type"
@@ -15,4 +15,3 @@ data "aws_ec2_instance_type_offerings" "my-instance-type-1" {
 output "v1-output-1" {
   value = data.aws_ec2_instance_type_offerings.my-instance-type-1.instance_types
 }
-*/
